36ac76a77e8943de43bce2033c96fc281635a44e
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-11-28 22:09:21 +0300

    implemented custom webpack configuration and installed quicklink and react router packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+// ray test touch <
+// import { BrowserRouter } from '@/components/Router';
+// ray test touch >
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ray test touch <
+ReactDOM.render(
+  // <BrowserRouter>
+  //   <App />
+  // </BrowserRouter>,
+  <App />,
+  document.getElementById('root')
+);
+// ray test touch >
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
