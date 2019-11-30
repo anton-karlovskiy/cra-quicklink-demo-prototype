23ffb21f553f1417039cb29913b753b1308fce15
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-11-30 22:28:13 +0300

    improved coding in ES6 and functional components and added License

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+// ray test touch <
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { useEffect } from 'react';
 import Loadable from 'react-loadable';
+
+// TODO: use hook instead of withRouter or might not needed at all
 import { Route, withRouter } from '@components/Router';
 import Footer from '@components/Footer';
 import Hero from '@components/Hero';
@@ -7,41 +26,44 @@ import style from './index.module.css';
 
 // Route-Split Components
 const loading = () => <div>Loading...</div>;
-const load = loader => Loadable({ loader, loading });
+const load = loader => Loadable({loader, loading});
 
 const Home = load(() => import('@pages/Home'));
 const About = load(() => import('@pages/About'));
 const Article = load(() => import('@pages/Article'));
 const Blog = load(() => import('@pages/Blog'));
 
-class App extends React.Component {
-	componentDidMount() {
+const App = ({ history }) => {
+	useEffect(() => {
 		// attach route manifest to global
-		fetch('/rmanifest.json').then(r => r.json()).then(data => {
-			window._rmanifest_ = data;
-		});
+		fetch('/rmanifest.json')
+			.then(response => response.json())
+			.then(data => {
+				window._rmanifest_ = data;
+			});
 
+		// TODO: might drop
 		if (process.env.NODE_ENV === 'production') {
-			this.props.history.listen(obj => {
+			history.listen(obj => {
 				if (window.ga) window.ga.send('pageview', {dp: obj.pathname});
 			});
 		}
-	}
+	// eslint-disable-next-line
+	}, []);
 
-	render() {
-		return (
-			<div className={ style.app }>
-				<Hero />
-				<main className={ style.wrapper }>
-					<Route path="/" exact component={ Home } />
-					<Route path="/blog" exact component={ Blog } />
-					<Route path="/blog/:title" component={ Article } />
-					<Route path="/about" exact component={ About } />
-				</main>
-				<Footer />
-			</div>
-		);
-	}
+	return (
+		<div className={style.app}>
+			<Hero />
+			<main className={style.wrapper}>
+				<Route path="/" exact component={Home} />
+				<Route path="/blog" exact component={Blog} />
+				<Route path="/blog/:title" component={Article} />
+				<Route path="/about" exact component={About} />
+			</main>
+			<Footer />
+		</div>
+	);
 }
 
 export default withRouter(App);
+// ray test touch >
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+// ray test touch <
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// TODO: convert to functional component
+import React, { Component } from 'react';
+
 import { Link } from '@components/Router';
 import style from './index.module.css';
 
-export default class Nav extends React.Component {
+class Nav extends Component {
 	state = {
 		stuck: false,
 	};
@@ -26,18 +45,20 @@ export default class Nav extends React.Component {
 		}
 
 		return (
-			<nav className={ cls }>
-				<ul className={ style.links }>
+			<nav className={cls}>
+				<ul className={style.links}>
 					<li><Link to="/">Home</Link></li>
 					<li><Link to="/blog">Blog</Link></li>
 					<li><Link to="/about">About</Link></li>
 				</ul>
-
 				<ul>
-					<li><a href="https://github.com/lukeed/pwa" className={ style.link_external }>GitHub</a></li>
-					<li><a href="https://github.com/lukeed/pwa" className={ style.link_external }>Documentation</a></li>
+					<li><a href="https://github.com/lukeed/pwa" className={style.link_external}>GitHub</a></li>
+					<li><a href="https://github.com/lukeed/pwa" className={style.link_external}>Documentation</a></li>
 				</ul>
 			</nav>
 		);
 	}
 }
+
+export default Nav;
+// ray test touch >
